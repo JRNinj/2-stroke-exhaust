e25617b418f79b1ccc98bb9f3ecfb494c809cbf6
JRNinj <JannickRichter@web.de>
2023-07-23 03:41:30 +0200

    entered whole GUI

--- run-calculator.py
+++ run-calculator.py
@@ -1,24 +1,118 @@
 import math
+import tkinter as tk
 
-# ALL DATA IN 'mm'
+def calc_exhaust():
+    # ALL DATA IN 'mm'
 
-outlet_diameter = 28 # Auslass Durchmesser
-temp_gases = 281.6 # Abgastemperatur in °C 200 - 300 °C normalerweise
-outlet_tax_time = 165 # Auslasssteuerzeit (Winkel mit dem sich die Kurbelwelle dreht in °)
-n = 9000 # Drehzahl in 1/min
-specific_usage_number = 12 # Variabler Wert, umso größer -> schmaleres Drehzahlband, umso kleiner -> breiteres Drehzahlband
+    if entry0 == None or entry1 == None or entry2 == None:
+        label4.config(text="Please enter the required data!")
+        return
+    else:
+        label4.config(text="")
+
+    ## Important Data
+    outlet_diameter = 28 # Auslass Durchmesser
+    outlet_tax_time = 130 # Auslasssteuerzeit (Winkel mit dem sich die Kurbelwelle dreht in °)
+    n = 6000 # Drehzahl in 1/min
+
+    ## Settings Data
+    temp_gases = 281.6 # Abgastemperatur in °C 200 - 300 °C normalerweise
+    pip_width_multiplicator = 14 # Variabler Wert, umso größer -> schmaleres Drehzahlband, umso kleiner -> breiteres Drehzahlband
+    manifold_multiplicator = 8 # ##Werte: 6 - 12## Variabler Wert, umso größer -> schmaleres Drehzahlband, umso kleiner -> breiteres Drehzahlband
+    pipe_multiplicator = 0.2 # ##Werte: 0.2 - 0.4## Variabler Wert
+
+    ## Calculation
+    outlet_surface = math.pi * math.pow((outlet_diameter / 2), 2) # Auslass Querschnittsfläche
+    manifold_width = math.sqrt((outlet_surface * 1.2) / math.pi) * 2 # Krümmerdurchmesser
+    manifold_lenght = manifold_width * manifold_multiplicator # Krümmerlänge
+    tailpipe_width = manifold_width * 0.55 # Endrohr Durchmesser
+    tailpipe_lenght = tailpipe_width * 12 # Endrohr Länge
+    c = 331 + 0.6 * temp_gases # Schallgeschwindigkeit
+    cone_counter_cone_length = ((c * outlet_tax_time) * 1000 / (12 * n)) - manifold_lenght # Länge von Konus bis 2/3 des Gegenkonus
+    cone_lenght = (cone_counter_cone_length / 1.23) * (1 - pipe_multiplicator) # Konuslänge
+    pipe_lenght = (cone_counter_cone_length / 1.23) * pipe_multiplicator # Länge des zylindrischen Rohres zw. Konus u. Gegenkonus
+    pipe_width = math.pow((pip_width_multiplicator * math.pow(manifold_width, 2)), 0.5) # Breite des Mittelstücks und Konus
+    counter_cone_lenght = cone_lenght * 0.5 # Gegenkonuslänge
+
+    print("Your Exhaust Data: \nManifold - Lenght: {:.2f} - Width: {:.2f} \nCone - Lenght: {:.2f} \nPipe - Lenght: {:.2f} - Width: {:.2f} \nCounter-Cone - Lenght: {:.2f} \nTailpipe - Lenght: {:.2f} - Width: {:.2f}".format(manifold_lenght, manifold_width, cone_lenght, pipe_lenght, pipe_width, counter_cone_lenght, tailpipe_lenght, tailpipe_width))
+
+window = tk.Tk()
+window.geometry("700x800")
+window.title("2-Stroke Exhaust Calculator   GUI")
+
+label = tk.Label(window, text="2-Stroke Exhaust Calculator", font=("Calibri", 25))
+label.pack(pady=10)
+
+canvas0 = tk.Canvas(window, width=800, height=800, relief='raised')
+canvas0.pack()
+
+# outlet diameter
+
+label1 = tk.Label(window, text="Outlet Diameter:")
+label1.config(font=('helvetica', 14))
+canvas0.create_window(150, 25, window=label1)
+
+entry0 = tk.Entry(window)
+canvas0.create_window(150, 65, window=entry0)
+
+# outlet tax time
+label2 = tk.Label(window, text="Outlet Tax Time:")
+label2.config(font=('helvetica', 14))
+canvas0.create_window(350, 25, window=label2)
+
+entry1 = tk.Entry(window)
+canvas0.create_window(350, 65, window=entry1)
+
+# rpm
+label3 = tk.Label(window, text="RPM:")
+label3.config(font=('helvetica', 14))
+canvas0.create_window(550, 25, window=label3)
+
+entry2 = tk.Entry(window)
+canvas0.create_window(550, 65, window=entry2)
+
+#Calculate Button
+
+button0 = tk.Button(window, text="Calculate Exhaust >", background="green", width=30, height=2, foreground="white", font=("Calibri", 14))
+canvas0.create_window(350, 200, window=button0)
+
+label4 = tk.Label(window, text="", foreground="red", font=("Calibri", 12))
+canvas0.create_window(350, 250, window=label4)
 
 
-outlet_surface = math.pi * math.pow((outlet_diameter / 2), 2) # Auslass Querschnittsfläche
-manifold_width = math.sqrt((outlet_surface * 1) / math.pi) * 2 # Krümmerdurchmesser
-manifold_lenght = manifold_width * 8 # Krümmerlänge
-tailpipe_width = manifold_width * 0.55 # Endrohr Durchmesser
-tailpipe_lenght = tailpipe_width * 12 # Endrohr Länge
-c = 331 + 0.6 * temp_gases # Schallgeschwindigkeit
-cone_counter_cone_length = ((c * outlet_tax_time) * 1000 / (12 * n)) - manifold_lenght # Länge von Konus bis 2/3 des Gegenkonus
-cone_lenght = (cone_counter_cone_length / 1.23) * 0.7 # Konuslänge
-pipe_lenght = (cone_counter_cone_length / 1.23) * 0.3 # Länge des zylindrischen Rohres zw. Konus u. Gegenkonus
-pipe_width = math.pow((specific_usage_number * math.pow(manifold_width, 2)), 0.5) # Breite des Mittelstücks und Konus
-counter_cone_lenght = cone_lenght * 0.5 # Gegenkonuslänge
+# data
+label_manifold_w = tk.Label(window, text="Manifold Width: ---", font=("Calibri", 14))
+canvas0.create_window(250, 300, window=label_manifold_w)
+label_manifold_l = tk.Label(window, text="Manifold Lenght: ---", font=("Calibri", 14))
+canvas0.create_window(250, 330, window=label_manifold_l)
 
-print("Your Exhaust Data: \nManifold - Lenght: {:.2f} - Width: {:.2f} \nCone - Lenght: {:.2f} \nPipe - Lenght: {:.2f} - Width: {:.2f} \nCounter-Cone - Lenght: {:.2f} \nTailpipe - Lenght: {:.2f} - Width: {:.2f}".format(manifold_lenght, manifold_width, cone_lenght, pipe_lenght, pipe_width, counter_cone_lenght, tailpipe_lenght, tailpipe_width))
+label_tailpipe_w = tk.Label(window, text="Tailpipe Width: ---", font=("Calibri", 14))
+canvas0.create_window(250, 380, window=label_tailpipe_w)
+label_tailpipe_l = tk.Label(window, text="Tailpipe Lenght: ---", font=("Calibri", 14))
+canvas0.create_window(250, 410, window=label_tailpipe_l)
+
+label_cone = tk.Label(window, text="Cone Length: ---", font=("Calibri", 14))
+canvas0.create_window(450, 300, window=label_cone)
+
+label_pipe_w = tk.Label(window, text="Pipe Width: ---", font=("Calibri", 14))
+canvas0.create_window(450, 340, window=label_pipe_w)
+label_pipe_l = tk.Label(window, text="Pipe Lenght: ---", font=("Calibri", 14))
+canvas0.create_window(450, 370, window=label_pipe_l)
+
+label_counter_cone = tk.Label(window, text="Counter-Cone Length: ---", font=("Calibri", 14))
+canvas0.create_window(450, 410, window=label_counter_cone)
+
+canvas0.create_rectangle(100, 500, 200, 515)
+canvas0.create_line(200, 500, 350, 470)
+canvas0.create_line(200, 515, 350, 545)
+canvas0.create_rectangle(350, 470, 400, 545)
+canvas0.create_line(400, 545, 475, 510)
+canvas0.create_line(400, 470, 475, 505)
+canvas0.create_rectangle(475, 505, 575, 510)
+
+#Exit Button
+
+button1 = tk.Button(window, text="Exit", background="gray", foreground="red", width=20, height=1, font=("Calibri", 14), command=window.quit)
+canvas0.create_window(350, 700, window=button1)
+
+window.mainloop()
